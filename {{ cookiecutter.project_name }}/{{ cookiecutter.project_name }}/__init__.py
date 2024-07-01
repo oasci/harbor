b1f900151fda18f374607368b28d2766dca80241
Alex Maldonado <alex@oasci.org>
2024-07-01 10:54:26 -0400

    chore: update to latest settings

--- {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/__init__.py
+++ {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/__init__.py
@@ -1,16 +1,16 @@
-# type: ignore[attr-defined]
-"""{{ cookiecutter.project_description }}"""
+__version__ = "0.0.0"
 
 import os
 import sys
 from ast import literal_eval
+from typing import Any
 
 from loguru import logger
 
-logger.disable("{{ cookiecutter.project_name }}")
+logger.disable("{{ cookiecutter.project_name.lower() }}")
 
 LOG_FORMAT = (
-    "<green>{time:HH:mm:ss}</green> | "
+    "<green>{time:YYYY-MM-DD HH:mm:ss.SSSZ}</green> | "
     "<level>{level: <8}</level> | "
     "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
 )
@@ -25,7 +25,7 @@ def enable_logging(
         level: Requested log level: `10` is debug, `20` is info.
         file_path: Also write logs to files here.
     """
-    config = {"handlers": []}
+    config: dict[str, Any] = {"handlers": []}
     if stdout_set:
         config["handlers"].append(
             {"sink": sys.stdout, "level": level_set, "format": LOG_FORMAT}
@@ -37,15 +37,11 @@ def enable_logging(
     # https://loguru.readthedocs.io/en/stable/api/logger.html#loguru._logger.Logger.configure
     logger.configure(**config)
 
-    logger.enable("{{ cookiecutter.project_name }}")
+    logger.enable("{{ cookiecutter.project_name.lower() }}")
 
 
 if literal_eval(os.environ.get("{{ cookiecutter.project_name.upper() }}_LOG", "False")):
     level = int(os.environ.get("{{ cookiecutter.project_name.upper() }}_LOG_LEVEL", 20))
-    stdout = literal_eval(
-        os.environ.get("{{ cookiecutter.project_name.upper() }}_STDOUT", "True")
-    )
-    log_file_path = os.environ.get(
-        "{{ cookiecutter.project_name.upper() }}_LOG_FILE_PATH", None
-    )
+    stdout = literal_eval(os.environ.get("{{ cookiecutter.project_name.upper() }}_STDOUT", "True"))
+    log_file_path = os.environ.get("{{ cookiecutter.project_name.upper() }}_LOG_FILE_PATH", None)
     enable_logging(level, stdout, log_file_path)
